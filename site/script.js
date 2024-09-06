7a56c726edf3613a736757fc852d8f6960550ca6
Deleminaro <ivan.solomakha.is@gmail.com>
2024-09-06 22:41:41 +1000

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,6 +1,7 @@
 // script.js
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
+const socket = io();
 
 const peerConnection = new RTCPeerConnection();
 
@@ -14,4 +15,32 @@ peerConnection.ontrack = event => {
     remoteVideo.srcObject = event.streams[0];
 };
 
-// Signaling logic to exchange SDP and ICE candidates goes here
+peerConnection.onicecandidate = event => {
+    if (event.candidate) {
+        socket.emit('candidate', event.candidate);
+    }
+};
+
+socket.on('offer', async (offer) => {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    socket.emit('answer', answer);
+});
+
+socket.on('answer', async (answer) => {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+});
+
+socket.on('candidate', async (candidate) => {
+    await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+});
+
+async function createOffer() {
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    socket.emit('offer', offer);
+}
+
+// Call createOffer() when you want to start the call
+createOffer();
